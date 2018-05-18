be62be3fa44fbe669a00cda01bd25e5b084a994f
Arain <liu2596615@163.com>
2018-05-18 16:19:30 +0800

    submit php

--- submit.php
+++ submit.php
@@ -1,9 +1,11 @@
 <html>
   <body>
     Welcome
-    <?php echo $_POST["name"]; ?>
+    <?php echo $_POST["price"]; ?>
     <br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+    DG Max Produce: <?php echo $_POST["DGMax"]; ?>
+    DG Min Produce: <?php echo $_POST["DGMin"]; ?>
+    <?php
     <?php
       if ($_FILES["file"]["error"] > 0) {
         echo "Error: " . $_FILES["file"]["error"] . "<br />";
@@ -13,6 +15,14 @@
         echo "Type: " . $_FILES["file"]["type"] . "<br />";
         echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
         echo "Stored in: " . $_FILES["file"]["tmp_name"];
+        if (file_exists("upload/" . $_FILES["file"]["name"])) {
+          echo $_FILES["file"]["name"] . " already exists. ";
+        }
+        else {
+          move_uploaded_file($_FILES["file"]["tmp_name"],
+          "upload/" . $_FILES["file"]["name"]);
+          echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+        }
       }
     ?>
 
